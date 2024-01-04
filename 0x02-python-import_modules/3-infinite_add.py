0a81cdf8778a86c2257b869052c18c00af6ff439
tgpaulos <warsidotigist11@gmail.com>
2024-01-04 14:29:07 -0800

    fixing errors in infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,12 +3,12 @@
 
 if __name__ == "__main__":
     import sys
-
-    args = sys.argv[1:]
     total = 0
+    args = sys.argv[1:]
+
 
     for arg in sys.argv:
         if arg != sys.argv[0]:
-        total += int(arg)
+            total += int(arg)
 
     print(total)
