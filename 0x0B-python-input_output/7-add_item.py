7fc95b986862899d293a6f28a8620686b0fbe932
tgpaulos <warsidotigist11@gmail.com>
2024-02-07 22:23:42 +0000

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""This function adds all arguments to a Python list and then save them to a file"""
+"""
+    This function adds all arguments to a Python list, and then save them to a file
+"""
 
 import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-from load_from_json_file import load_from_json_file
-from save_to_json_file import save_to_json_file
 
-def add_arguments_to_list(arguments):
+if __name__ == "__main__":
+
+    filename = "add_item.json"
+
     try:
-        existing_list = load_from_json_file('add_item.json')
-    except FileNotFoundError:
-        existing_list = []
+        arg_list = load_from_json_file(filename)
+    except:
+        arg_list = []
 
-    existing_list.extend(arguments)
-
-    save_to_json_file(existing_list, 'add_item.json')
-
-if __name__ == '__main__':
-    arguments = sys.argv[1:]
-    add_arguments_to_list(arguments)
+    for arg in sys.argv[1:]:
+        arg_list.append(arg)
+    save_to_json_file(arg_list, filename)
