09bf8a4214eb0279886f03b559b98a615aefbffd
tgpaulos <warsidotigist11@gmail.com>
2024-02-07 20:33:23 +0000

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-
-"""defining write_file with two arguments"""
+"""
+    1-write_file: write_file()
+"""
 
 
 def write_file(filename="", text=""):
-
-    """reads filename with utf-8"""
-    with open(filename, "w", encoding='utf-8') as f:
-      return f.write(text)
-        
+    """
+        write_file writes a string to a text file.
+        Args:
+            filename (str): name of file.
+            text (str): text to be written.
+        Returns: number of bytes written.
+    """
+    with open(filename, "w", encoding='utf-8') as a_file:
+        return a_file.write(text)       
