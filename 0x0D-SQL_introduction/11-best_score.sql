0b8543708e6e187f2c6519923dc415421ee93104
tgpaulos <warsidotigist11@gmail.com>
2024-03-14 15:37:35 -0700

    from task#17 to task 20

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+--lists all rows of the table second_table
+--Results should display both the score and the name (in this order)
+--Records should be ordered by score (top first)
+--The database name will be passed as an argument of the mysql command
+
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
